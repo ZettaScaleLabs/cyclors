31a54476ed07bb6d8af8e0d82f016ed4f27aad8b
kydos <angelo@icorsaro.net>
2021-02-17 23:08:47 +0100

    allow configuration of cyclone installation through CYCLONE_INCLUDE and CYCLONE_LIB env vars

--- build.rs
+++ build.rs
@@ -6,9 +6,16 @@ use std::path::PathBuf;
 fn main() {
     println!("cargo:rustc-link-lib=ddsc");
     println!("cargo:rustc-link-lib=cdds-util");
+    let incl_dir = if let Ok(path) = env::var("CYCLONE_INCLUDE") {
+    	format!("-I{}", path)
+    } else { "-I/usr/local/include".into() };
+    let lib_dir = if let Ok(path) = env::var("CYCLONE_INCLUDE") {
+    	format!("-L{}", path)
+    } else { "-L/usr/local/include".into() };
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-I/usr/local/include")
+	.clang_arg(incl_dir)
+	.clang_arg(lib_dir)
         .generate_comments(false)
         .generate()
         .expect("Unable to generate bindings");
