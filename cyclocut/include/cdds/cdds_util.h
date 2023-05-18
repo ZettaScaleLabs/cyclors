69e14ce325d4973b599d5044f8b25a2750a679eb
Geoff Martin <geoff.martin@zettascale.tech>
2023-05-18 12:47:21 +0100

    Moving cyclocut code into cyclors repository.

---
+++ cyclocut/include/cdds/cdds_util.h
@@ -0,0 +1,20 @@
+#ifndef ATOLAB_CDDS_UTIL_H_
+#define ATOLAB_CDDS_UTIL_H_
+
+
+#include "dds/dds.h"
+#include "dds/ddsi/ddsi_serdata.h"
+#include "dds/ddsi/ddsi_radmin.h"
+
+//#define CY_DEBUG_ON 1
+#ifdef CY_DEBUG_ON
+    #define CY_DEBUG(msg) printf(msg)
+    #define CY_DEBUG_WA(fmt, args...) printf(fmt, args)
+#else
+    #define CY_DEBUG(msg)
+    #define CY_DEBUG_WA(fmt, args...)
+#endif
+
+dds_entity_t cdds_create_blob_topic(dds_entity_t dp, char *topic_name, char* type_name, bool is_keyless);
+
+#endif /* ATOLAB_CDDS_UTIL_H_ */
