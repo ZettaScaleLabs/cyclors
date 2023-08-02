c97d4d7780ba5ef4dd3c92202340e7ea85f7f3fa
Geoff Martin <geoff.martin@zettascale.tech>
2023-08-02 13:43:44 +0100

    Iceoryx is now optional through the iceoryx feature.

--- build.rs
+++ build.rs
@@ -8,65 +8,81 @@ fn main() {
     let mut dir_builder = std::fs::DirBuilder::new();
     dir_builder.recursive(true);
 
-    // Build iceoryx
-    let iceoryx_dir = out_dir.join("iceoryx-build");
-    dir_builder.create(&iceoryx_dir).unwrap();
-    let mut iceoryx = cmake::Config::new("iceoryx/iceoryx_meta");
-
-    // Force compilation of Iceoryx in release mode on Windows due to
-    // https://github.com/rust-lang/rust/issues/39016
-    #[cfg(all(debug_assertions, target_os = "windows"))]
-    let iceoryx = iceoryx.profile("Release");
-
-    let iceoryx = iceoryx
-        .define("BUILD_SHARED_LIBS", "OFF")
-        .out_dir(iceoryx_dir)
-        .build();
-
-    let iceoryx_lib = iceoryx.join("lib");
-
-    // Add iceoryx lib to link
-    println!("cargo:rustc-link-search=native={}", iceoryx_lib.display());
-    println!("cargo:rustc-link-lib=static=iceoryx_binding_c");
-    println!("cargo:rustc-link-lib=static=iceoryx_hoofs");
-    println!("cargo:rustc-link-lib=static=iceoryx_posh");
-    println!("cargo:rustc-link-lib=static=iceoryx_platform");
-
-    #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-lib=acl");
-
-    #[cfg(not(any(target_os = "windows", target_os = "macos")))]
-    println!("cargo:rustc-link-lib=stdc++");
-
-    #[cfg(any(target_os = "macos"))]
-    println!("cargo:rustc-link-lib=c++");
-
-    let iceoryx_install_path = iceoryx.as_os_str();
-
-    // Build cyclonedds
-    let cyclonedds_dir = out_dir.join("cyclonedds-build");
-    dir_builder.create(&cyclonedds_dir).unwrap();
+    // Create initial Cyclone DDS build config
     let mut cyclonedds = cmake::Config::new("cyclonedds");
+    let mut cyclonedds = cyclonedds
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .define("BUILD_IDLC", "OFF")
+        .define("BUILD_DDSPERF", "OFF")
+        .define("ENABLE_LTO", "NO")
+        .define("ENABLE_SSL", "NO")
+        .define("ENABLE_SECURITY", "NO")
+        .define("CMAKE_INSTALL_LIBDIR", "lib");
+
+    // Create initial bindings builder
+    let mut bindings = bindgen::Builder::default();
+
+    #[cfg(feature = "iceoryx")]
+    {
+        // Build iceoryx
+        let iceoryx_dir = out_dir.join("iceoryx-build");
+        dir_builder.create(&iceoryx_dir).unwrap();
+        let mut iceoryx = cmake::Config::new("iceoryx/iceoryx_meta");
+
+        // Force compilation of Iceoryx in release mode on Windows due to
+        // https://github.com/rust-lang/rust/issues/39016
+        #[cfg(all(debug_assertions, target_os = "windows"))]
+        let iceoryx = iceoryx.profile("Release");
+
+        let iceoryx = iceoryx
+            .define("BUILD_SHARED_LIBS", "OFF")
+            .out_dir(iceoryx_dir)
+            .build();
+
+        let iceoryx_lib = iceoryx.join("lib");
+        let iceoryx_include = iceoryx.join("include/iceoryx/v2.0.3");
+
+        // Add iceoryx lib to link
+        println!("cargo:rustc-link-search=native={}", iceoryx_lib.display());
+        println!("cargo:rustc-link-lib=static=iceoryx_binding_c");
+        println!("cargo:rustc-link-lib=static=iceoryx_hoofs");
+        println!("cargo:rustc-link-lib=static=iceoryx_posh");
+        println!("cargo:rustc-link-lib=static=iceoryx_platform");
+
+        let iceoryx_install_path = iceoryx.as_os_str();
+
+        cyclonedds = cyclonedds
+            .env("iceoryx_binding_c_DIR", iceoryx_install_path)
+            .env("iceoryx_hoofs_DIR", iceoryx_install_path)
+            .env("iceoryx_posh_DIR", iceoryx_install_path)
+            .define("ENABLE_SHM", "YES");
+
+        bindings = bindings
+            .clang_arg(format!("-I{}", iceoryx_include.to_str().unwrap()))
+            .clang_arg("-DDDS_HAS_SHM=1");
+
+        #[cfg(target_os = "linux")]
+        println!("cargo:rustc-link-lib=acl");
+
+        #[cfg(not(any(target_os = "windows", target_os = "macos")))]
+        println!("cargo:rustc-link-lib=stdc++");
+
+        #[cfg(any(target_os = "macos"))]
+        println!("cargo:rustc-link-lib=c++");
+    }
 
     // Force compilation of Cyclone DDS in release mode on Windows due to
     // https://github.com/rust-lang/rust/issues/39016
     #[cfg(all(debug_assertions, target_os = "windows"))]
     let cyclonedds = cyclonedds.profile("Release");
 
-    let cyclonedds = cyclonedds
-        .define("BUILD_SHARED_LIBS", "OFF")
-        .define("BUILD_IDLC", "OFF")
-        .define("BUILD_DDSPERF", "OFF")
-        .define("ENABLE_LTO", "NO")
-        .define("ENABLE_SHM", "YES")
-        .define("ENABLE_SSL", "NO")
-        .define("ENABLE_SECURITY", "NO")
-        .define("CMAKE_INSTALL_LIBDIR", "lib")
-        .env("iceoryx_binding_c_DIR", iceoryx_install_path)
-        .env("iceoryx_hoofs_DIR", iceoryx_install_path)
-        .env("iceoryx_posh_DIR", iceoryx_install_path)
-        .out_dir(cyclonedds_dir)
-        .build();
+    // Build cyclonedds
+    let cyclonedds_dir = out_dir.join("cyclonedds-build");
+    dir_builder.create(&cyclonedds_dir).unwrap();
+
+    cyclonedds = cyclonedds.out_dir(cyclonedds_dir);
+
+    let cyclonedds = cyclonedds.build();
 
     let cyclonedds_include = cyclonedds.join("include");
     let cyclonedds_lib = cyclonedds.join("lib");
@@ -112,7 +128,7 @@ fn main() {
     println!("cargo:rustc-link-lib=static=cdds-util");
 
     // Generate bindings
-    let bindings = bindgen::Builder::default()
+    bindings = bindings
         .header("wrapper.h")
         .clang_arg(format!("-I{}", cyclonedds_include.to_str().unwrap()))
         .clang_arg(format!("-I{}", cyclocut_include.to_str().unwrap()))
