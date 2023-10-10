ada1c5eda04a4867c6c4e16eb33fddb6f6c565da
Geoff Martin <geoff.martin@zettascale.tech>
2023-10-10 13:26:23 +0100

    Adding loaned sample and PSMX headers for bindgen generation.

--- wrapper.h
+++ wrapper.h
@@ -1,6 +1,8 @@
 #include <string.h>
 
 #include <dds/dds.h>
+#include <dds/ddsc/dds_loaned_sample.h>
+#include <dds/ddsc/dds_psmx.h>
 #include <dds/ddsi/ddsi_serdata.h>
 #include <dds/ddsi/ddsi_typelib.h>
 
