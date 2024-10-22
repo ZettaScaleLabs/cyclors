a40c442b444295b2fa03656a4530e8ce89164cb0
Geoff Martin <geoff.martin@zettascale.tech>
2024-10-22 17:51:27 +0100

    objcopy and nm commands now executed via cmake to use architecture specific tool.

---
+++ objcopy/check_errors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -s $1 ]; then
+    echo 'Command succeeded with no stderr output.'
+    exit 0
+else
+    echo 'Command failed with errors:'
+    cat stderr.txt
+    exit 1
+fi
